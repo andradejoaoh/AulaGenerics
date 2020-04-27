1be8af2f25b677f88308060781697d12e195c99d
João Henrique Andrade <andradejoaoh@gmail.com>
2020-04-27 17:59:19 -0300

    Exercícios Júlio

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -18,6 +18,42 @@ pilha.push(intA)
 pilha.push(intB)
 
 //TODO: Fazer um método que recebe um Array e um elemento, retorna um Bool dizendo se o elemento se encontra no Array
+func isOnList<T: Equatable>(array: Array<T>, element: T) -> Bool{
+    return array.contains(element)
+}
+
 //TODO: Extensão de Array contendo uma função que retorna a soma de todos os elementos, independente de qual o valor numérico
+extension Array where Element : Numeric {
+    func sum() -> Element{
+        var soma = Element.zero
+        
+        for element in self {
+            soma += element
+        }
+        return soma
+    }
+}
+
+
 //TODO: Fazer um método que recebe 2 Arrays e retorna um Int referente a quantidade de elementos presentes em ambos os Arrays
     //BONUS TODO: Fazer o método acima receber N arrays
+func isComun<T: Hashable>(firstArray: [T], secondArray: [T]) -> Int{
+    let firstSet: Set<T> = Set(firstArray)
+    let secondSet: Set<T> = Set(secondArray)
+    
+    return firstSet.intersection(secondSet).count
+    }
+
+func isComunAnyElements<T: Hashable>(arrays: [T]...) -> Int{
+    var result = 0
+    for numbersInArray in 0..<arrays.first!.count{
+        for array in arrays {
+            if !array.contains(arrays[0][numbersInArray]){
+                break;
+            }
+        }
+        result += 1
+    }
+    return result
+}
+
